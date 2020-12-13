c289822a638791c16e201e515afa96d6d0c99812
yxd <411867545@qq.com>
2020-12-13 13:03:55 +0800

    下载功能实现

--- src/main/java/com/mizhi/yxd/controller/dealcontroller/PoorController.java
+++ src/main/java/com/mizhi/yxd/controller/dealcontroller/PoorController.java
@@ -8,7 +8,6 @@ import com.mizhi.yxd.service.PoorService;
 import com.mizhi.yxd.tools.BeanUtils;
 import com.mizhi.yxd.tools.ExcelUtils;
 import com.mizhi.yxd.tools.Layui;
-import com.mizhi.yxd.tools.SnowflakeIdWorker;
 import com.mizhi.yxd.validate.ValueValidate;
 import com.mizhi.yxd.vo.PoorExportVo;
 import com.mizhi.yxd.vo.PoorVo;
@@ -16,11 +15,18 @@ import com.mizhi.yxd.vo.UpdatePoorVo;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -113,5 +119,18 @@ public class PoorController {
         poorService.insertPoorInfo(subPoor);
         log.info("add poor success");
         return Result.success("success");
+    }
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> downloadTemp(HttpServletRequest request) throws IOException{
+        String path = getClass().getResource("/download/poor.xlsx").getPath();
+        String newFileName = "贫困库模板.xlsx";
+        File file = FileUtils.getFile(path);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", new String(
+                newFileName.getBytes("gbk"), "iso-8859-1"));
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
+                headers, HttpStatus.OK);
     }
 }
