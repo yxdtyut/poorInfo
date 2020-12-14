9e40f2ab9a038f6f01115660f2c47c1e92e7b2de
yxd <411867545@qq.com>
2020-12-14 23:03:26 +0800

    跳转至资助库页面

--- src/main/java/com/mizhi/yxd/controller/viewcontroller/ViewRoute.java
+++ src/main/java/com/mizhi/yxd/controller/viewcontroller/ViewRoute.java
@@ -16,7 +16,6 @@ public class ViewRoute {
         return "login";
     }
 
-
     @RequestMapping("/loginOut")
     public String loginOut(HttpSession httpSession) {
         httpSession.removeAttribute("name");
@@ -30,21 +29,6 @@ public class ViewRoute {
             return "adminIndex";
     }
 
-    @RequestMapping("/stuIndex")
-    public String stuIndex() {
-        return "stuIndex";
-    }
-
-    @RequestMapping("/teacherIndex")
-    public String teacherIndex() {
-        return "teacherIndex";
-    }
-
-    @RequestMapping("/teacherInfo")
-    public String teacherInfo() {
-        return "teacherInfo";
-    }
-
     @RequestMapping("/poorInfo/add")
     public String poorInfoAdd() {
         return "poorInfoAdd";
@@ -55,89 +39,9 @@ public class ViewRoute {
         return "poorInfo";
     }
 
-    @RequestMapping("/stuInfo_mf")
-    public String stuInfo_mf() {
-        return "stuInfo_mf";
-    }
-
-
-    @RequestMapping("/parseStu")
-    public String parseStu() {
-        return "parseStu";
-    }
-
     @RequestMapping("/registView")
-    public String StuRegister() {
+    public String poorRegister() {
         return "registView";
-    }
-
-    @RequestMapping("/stuModi")
-    public ModelAndView stuModi(ModelAndView mav, @RequestParam("num") String num) {
-        mav.addObject("num", num);
-        mav.setViewName("stuModi");
-        return mav;
-    }
-
-
-    @RequestMapping("/stuScore")
-    public String stuScore() {
-        return "stuScore";
-    }
-
-
-    @RequestMapping("/teaAdd")
-    public String teaAdd() {
-        return "teaAdd";
-    }
-
-    @RequestMapping("/teacherInfo_mf")
-    public String teacherInfo_mf() {
-        return "teacherInfo_mf";
-    }
-
-
-    @RequestMapping("/teaModi")
-    public ModelAndView teaModi(ModelAndView mav, @RequestParam("num") String num) {
-        mav.addObject("num", num);
-        mav.setViewName("teaModi");
-        return mav;
-    }
-
-
-    @RequestMapping("/claCouModi")
-    public ModelAndView claCouModi(ModelAndView mav, @RequestParam("coursename") String coursename, @RequestParam("num") String num) {
-        mav.addObject("num", num);
-        System.out.println(coursename);
-        mav.addObject("coursename", coursename);
-        mav.setViewName("claCouModi");
-        return mav;
-    }
-
-
-    @RequestMapping("/claCouScore")
-    public String claCouScore() {
-        return "claCouScore";
-    }
-
-    @RequestMapping("/ClaCouSco")
-    public String ClaCouSco() {
-        return "ClaCouSco";
-    }
-
-    @RequestMapping("/ClaCouTea")
-    public String ClaCouTea() {
-        return "ClaCouTea";
-    }
-
-    @RequestMapping("/parseClaCouSco")
-    public String parseClaCouSco() {
-        return "parseClaCouSco";
-    }
-
-
-    @RequestMapping("/parseClaComp")
-    public String parseClaComp() {
-        return "parseClaComp";
     }
 
     @RequestMapping("/welcome")
@@ -145,10 +49,8 @@ public class ViewRoute {
         return "welcome";
     }
 
-    @RequestMapping("/announce")
-    public String announce() {
-        return "announce";
+    @RequestMapping("/subsidize")
+    public String subsidize() {
+        return "subsidize";
     }
-
-
 }
