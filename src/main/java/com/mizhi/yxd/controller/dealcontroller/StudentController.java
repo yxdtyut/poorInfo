a52c6b7625381c45304a6a95dd60ec42398b9ca5
yxd <411867545@qq.com>
2020-12-20 22:42:49 +0800

    在校学生下载模板功能

--- src/main/java/com/mizhi/yxd/controller/dealcontroller/StudentController.java
+++ src/main/java/com/mizhi/yxd/controller/dealcontroller/StudentController.java
@@ -11,10 +11,18 @@ import com.mizhi.yxd.vo.PoorExportVo;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -54,5 +62,18 @@ public class StudentController {
         studentService.insertPoorInfo(subUser);
         log.info("add student success");
         return Result.success("success");
+    }
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> downloadTemp(HttpServletRequest request) throws IOException {
+        String path = getClass().getResource("/download/student.xlsx").getPath();
+        String newFileName = "在校学生模板.xlsx";
+        File file = FileUtils.getFile(path);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", new String(
+                newFileName.getBytes("gbk"), "iso-8859-1"));
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
+                headers, HttpStatus.OK);
     }
 }
